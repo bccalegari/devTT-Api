2c7eafe3ad322dd7ce0f1a6a70d2240267c57865
bccalegari <brunocostacallegari@gmail.com>
2024-05-05 15:26:23 -0300

    feat: first commit
    initial setup

---
+++ src/main/java/br/com/devTT/DevTtApplication.java
@@ -0,0 +1,13 @@
+package br.com.devTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTtApplication.class, args);
+	}
+
+}
